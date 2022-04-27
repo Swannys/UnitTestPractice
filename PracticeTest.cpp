23005fb09500b47a0491ebb41fc0957ea696623f
Swannys <bswan2018@yahoo.com>
2022-04-27 12:31:00 -0700

    Fixed functions and added multiple tests for each function

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -18,3 +18,51 @@ TEST(PracticeTest, smoke_test)
 {
     ASSERT_TRUE(true);
 }
+
+TEST(PracticeTest, allnighter_zeros)
+{
+	Practice TestObject;
+	int sleep[7] = {0,0,0,0,0,0,0};
+
+	ASSERT_EQ(TestObject.allnighter(sleep), &sleep[0]);
+}
+
+TEST(PracticeTest, sort_ascending_a) {
+	Practice TestObject;
+	int a = 7;
+	int b = 8;
+	int c = 9;
+
+	TestObject.sortDescending(a,b,c);
+	ASSERT_EQ(a, 9);
+}
+TEST(PracticeTest, sort_ascending_b) {
+	Practice TestObject;
+	int a = 7;
+	int b = 8;
+	int c = 9;
+
+	TestObject.sortDescending(a,b,c);
+	ASSERT_EQ(b, 8);
+}
+TEST(PracticeTest, sort_ascending_c) {
+	Practice TestObject;
+	int a = 7;
+	int b = 8;
+	int c = 9;
+
+	TestObject.sortDescending(a,b,c);
+	ASSERT_EQ(c, 7);
+}
+
+TEST(PracticeTest, not_palindrome) {
+	Practice TestObject;
+	string test = "tacocat";
+	ASSERT_EQ(TestObject.isPalindrome(test), true);
+}
+
+TEST(PracticeTest, count_repeat) {
+	Practice TestObject;
+	string test = "testing count";
+	ASSERT_EQ(TestObject.count_starting_repeats(test), 2);
+}
